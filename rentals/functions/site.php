abe754ebfdead4e9f1e3f9e2709e84e4f7ee1cc4
DESKTOP-5UHRTVE\jumwa <jjumwa@sevenseastech.com>
2018-07-13 18:44:03 +0300

    site URL

---
+++ rentals/functions/site.php
@@ -0,0 +1,26 @@
+<?php   
+    
+    $site       = array(
+                        'url'       => 'http://localhost/estate/rentals/' 
+                    );
+    $site2       = array(
+                        'url'       => 'http://localhost/estate/' 
+                    );
+                    
+    $address    = array(
+                        'box'       => 'P. O. Box 67964',
+                        'code'      => '00200',
+                        'city'      => 'Nairobi',
+                        'country'   => 'Kenya',
+                        'physical'  => 'Kitengela.'
+                    );
+                    
+    $contact    = array(
+                        'tel1'      => '(+254) 723 114 097',
+                        'tel2'      => '(+254) 702 644 670',
+                        
+                    );
+                    
+    $email      = array(
+                        'info'      => '<a href="mailto:jumwa@outlook.com">jumwa@outlook.com</a>'
+                    );
